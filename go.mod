4f218c98bd48637b67c878b843bfd49dda008d24
yulijun <koalaylj@gmail.com>
2021-11-09 05:35:02 +0800

    add pdf generator

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/koalaylj/go-mental-arithmetic
 
 go 1.16
+
+require github.com/johnfercher/maroto v0.33.0
--- src/main.go
+++ src/main.go
@@ -3,34 +3,167 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
+
+	"github.com/johnfercher/maroto/pkg/consts"
+	"github.com/johnfercher/maroto/pkg/pdf"
+	"github.com/johnfercher/maroto/pkg/props"
 )
 
 var ops = [...]string{"+", "-"}
 
 const max = 10
 const min = 1
-const total = 60
+const ARG_PAGE_SIZE = 60
+const ARG_COL = 4
+const ARG_PAGES = 3
 
 func main() {
 
-	for i := 1; i <= total; i++ {
+	page := [][]string{}
 
-		random := getRandom()
+	for row_i := 0; row_i < ARG_PAGE_SIZE/ARG_COL; row_i++ {
 
-		op := randomOp(random)
+		row := []string{}
 
-		item := ""
+		for col_i := 0; col_i < ARG_COL; col_i++ {
+			no := row_i + col_i*ARG_PAGE_SIZE/ARG_COL + 1
 
-		switch op {
-		case "+":
-			item = randomAdd(random, min, max)
-		case "-":
-			item = randomSub(random, min, max)
+			random := getRandom()
+
+			op := randomOp(random)
+
+			item := ""
+
+			switch op {
+			case "+":
+				item = randomAdd(random, min, max)
+			case "-":
+				item = randomSub(random, min, max)
+			}
+
+			cell := fmt.Sprintf("%2d) %s\n", no, item)
+
+			row = append(row, cell)
 		}
 
-		fmt.Printf("%2d) %s\n", i, item)
+		page = append(page, row)
 	}
+
+	// testPDF()
+	toPDF(page)
+}
+
+func testPDF() {
+	begin := time.Now()
+	m := pdf.NewMaroto(consts.Portrait, consts.Letter)
+	m.SetBorder(false)
+
+	m.Row(40, func() {
+		m.Col(3, func() {
+			m.Text("Any Text1")
+		})
+		m.Col(3, func() {
+			m.Text("Any Text2")
+		})
+		m.Col(3, func() {
+			m.Text("Any Text3")
+		})
+		m.Col(3, func() {
+			m.Text("Any Text3")
+		})
+	})
+
+	err := m.OutputFileAndClose("./textgrid.pdf")
+	if err != nil {
+		fmt.Println("Could not save PDF:", err)
+		os.Exit(1)
+	}
+
+	end := time.Now()
+	fmt.Println(end.Sub(begin))
+}
+
+func toPDF(page [][]string) {
+	begin := time.Now()
+	m := pdf.NewMaroto(consts.Portrait, consts.A4)
+	m.SetBorder(false)
+
+	newPage(m, page)
+
+	// for i := 0; i < 10; i++ {
+	// 	// newPage(m,[])
+
+	// 	if i < 9 {
+	// 		m.AddPage()
+	// 	}
+	// }
+
+	err := m.OutputFileAndClose("./fuck.pdf")
+	if err != nil {
+		fmt.Println("Could not save PDF:", err)
+		os.Exit(1)
+	}
+
+	end := time.Now()
+	fmt.Println(end.Sub(begin))
+}
+
+func newPage(m pdf.Maroto, cells [][]string) {
+
+	for i := 0; i < ARG_PAGE_SIZE/ARG_COL; i++ {
+		// for j := 0; j < ARG_COL; j++ {
+		// fmt.Printf("a[%d][%d] = %s\n", i, j, page[i][j])
+		m.Row(15, func() {
+			m.Col(3, func() {
+				m.Text(cells[i][0], props.Text{
+					Style: consts.Normal,
+					Align: consts.Left,
+					Size:  14,
+				})
+			})
+			m.Col(3, func() {
+				m.Text(cells[i][1], props.Text{
+					Style: consts.Normal,
+					Align: consts.Left,
+					Size:  14,
+				})
+			})
+			m.Col(3, func() {
+				m.Text(cells[i][2], props.Text{
+					Style: consts.Normal,
+					Align: consts.Left,
+					Size:  14,
+				})
+			})
+			m.Col(3, func() {
+				m.Text(cells[i][3], props.Text{
+					Style: consts.Normal,
+					Align: consts.Left,
+					Size:  14,
+				})
+			})
+		})
+
+		// }
+	}
+
+	// m.Row(40, func() {
+	// 	m.Col(3, func() {
+	// 		m.Text("Any Text1")
+	// 	})
+	// 	m.Col(3, func() {
+	// 		m.Text("Any Text2")
+	// 	})
+	// 	m.Col(3, func() {
+	// 		m.Text("Any Text3")
+	// 	})
+	// 	m.Col(3, func() {
+	// 		m.Text("Any Text3")
+	// 	})
+	// })
+
 }
 
 func randomAdd(random *rand.Rand, min int, max int) string {
@@ -78,7 +211,7 @@ func randomSub(random *rand.Rand, min int, max int) string {
 }
 
 func render(a int, b int, op string) string {
-	return fmt.Sprintf("%2d %s %d", a, op, b)
+	return fmt.Sprintf("%2d %s %d =", a, op, b)
 }
 
 func randomOp(random *rand.Rand) string {
